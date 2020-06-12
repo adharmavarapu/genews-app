bc183d2d92925b6fb0659873af9f9cdcd60fef2e
adharmavarapu <adharmavarapu@gmail.com>
2020-06-12 14:52:44 -0500

    Top bar margin update

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,8 +1,5 @@
 import { Platform, StatusBar } from "react-native";
 
 export default {
-  TOP_MARGIN:
-    Platform.OS == "android" && StatusBar.currentHeight > 24
-      ? StatusBar.currentHeight + 10
-      : 10,
+  TOP_MARGIN: Platform.OS == "android" ? StatusBar.currentHeight + 10 : 10,
 };
